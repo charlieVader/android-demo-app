69de7d9d5d579b10ae63195c616b6f73a689f48a
Francis Martinez <francis.martinez@bonzzu.com>
2023-07-26 11:58:49 -0500

    create activity with counter

--- app/src/main/java/com/bonzzu/demo/MainActivity.kt
+++ app/src/main/java/com/bonzzu/demo/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.bonzzu.demo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bonzzu.demo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var counter = 0
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        updateCounterFragment()
+        setEvents()
+    }
+
+    private fun setEvents() {
+        binding.btnIncrease.setOnClickListener {
+            counter++
+            updateCounterFragment()
+        }
+
+        binding.btnDecrease.setOnClickListener {
+            if (counter == 0) return@setOnClickListener
+
+            counter--
+            updateCounterFragment()
+        }
+    }
+
+    private fun updateCounterFragment() {
+        binding.tvCounter.text = counter.toString()
+        binding.tvBodyCounter.text = counter.toString()
     }
 }
