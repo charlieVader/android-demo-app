865dc245243ca4158e44268a0cac7c8359b13d1f
Francis Martinez <francis.martinez@bonzzu.com>
2023-08-17 11:28:09 -0500

    Update exercises file

--- app/src/main/java/com/bonzzu/demo/util/Util.kt
+++ app/src/main/java/com/bonzzu/demo/util/Util.kt
@@ -3,17 +3,8 @@ package com.bonzzu.demo.util
 object Util {
 
     //Exercise 1
-    //Refactor the following function
-    fun calculateSum(numbers: List<Int>): Int {
-        var result = 0
-        for (i in 0 until numbers.size) {
-            result += numbers[i]
-        }
-        return result
-    }
-
-    //Exercise 2
-    //Find the errors
+    //Copy the following exercise in https://play.kotlinlang.org/
+    //and find and fix the errors
     fun calculateAverage(numbers: List<Int>): Double {
         var sum = 0
         for (i in 1..numbers.size) {
@@ -22,8 +13,18 @@ object Util {
         return sum / numbers.size
     }
 
+    //Exercise 2
+    //Given a list of numbers, filter out the even numbers and then
+    //double each remaining number using the filter and map functions.
+
     //Exercise 3
-    //Implement a function that makes an HTTP request using Retrofit
+    //Imagine you have a list of bank accounts, each represented by a data class BankAccount
+    // with properties accountNumber and balance.
+    // Write a Kotlin program that uses the reduce function to calculate the total balance
+    // across all bank accounts.
+
+    //Exercise 4
+    //Implement a function that makes an HTTP request using Retrofit to get the name and game_index,
     //handling possible errors and success responses
     //You can use this API -> https://pokeapi.co/
 
